9e9c1629246614148d78f02184b6021ba7d034de
HD-theEngineer <duongghuy96@gmail.com>
2025-09-28 16:34:28 +0700

    Update: add null handle for dim_date model.

--- glamira_analytics/models/core/dim_date.sql
+++ glamira_analytics/models/core/dim_date.sql
@@ -10,14 +10,39 @@ WITH raw_timeline AS (
   FROM UNNEST(GENERATE_DATE_ARRAY((SELECT first_date FROM raw_timeline), (SELECT last_date FROM raw_timeline), INTERVAL 1 DAY)) AS day
 )
 
+, dim_date__modified AS (
+  SELECT
+    CAST(FORMAT_DATE('%Y%m%d', date) AS INTEGER) AS date_key,
+    date,
+    EXTRACT(DAY FROM date) AS day,
+    FORMAT_DATE('%A', date) AS day_name,
+    EXTRACT(ISOWEEK FROM date) AS week_number,
+    EXTRACT(MONTH FROM date) AS month,
+    FORMAT_DATE('%B', date) AS month_name,
+    EXTRACT(YEAR FROM date) AS year,
+    CASE WHEN EXTRACT(DAYOFWEEK FROM date) IN (1,7) THEN TRUE ELSE FALSE END AS is_weekend
+  FROM dates
+)
+
+, dim_date__invalid_handle AS (
+  SELECT
+    *
+  FROM
+    dim_date__modified
+  UNION ALL
+  SELECT
+    -1 AS date_key,
+    CAST(NULL AS DATE) AS date,
+    -1 AS day,
+    'XNA' AS day_name,
+    -1 AS week_number,
+    -1 AS month,
+    'XNA' AS month_name,
+    -1 AS year,
+    CAST(NULL AS BOOLEAN) AS is_weekend,
+)
+
 SELECT
-  FORMAT_DATE('%Y%m%d', date) AS date_key,
-  date,
-  EXTRACT(DAY FROM date) AS day,
-  FORMAT_DATE('%A', date) AS day_name,
-  EXTRACT(ISOWEEK FROM date) AS week,
-  EXTRACT(MONTH FROM date) AS month,
-  FORMAT_DATE('%B', date) AS month_name,
-  EXTRACT(YEAR FROM date) AS year,
-  CASE WHEN EXTRACT(DAYOFWEEK FROM date) IN (1,7) THEN TRUE ELSE FALSE END AS is_weekend
-FROM dates
+  *
+FROM
+  dim_date__invalid_handle
